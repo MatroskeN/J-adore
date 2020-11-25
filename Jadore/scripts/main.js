60d6542aa75a62565f05041ba59ff9a3449c21e9
BogdanDelukin <makaev.v1d@gmail.com>
2020-11-25 14:24:57 +0400

    fefef

---
+++ Jadore/scripts/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: false,
+        draggable: true,
+        snapOnRelease: false,
+    },
+});
