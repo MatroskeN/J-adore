ccba28b85e1f80fbbafbaf4a3dcfa97ac3c88f63
BogdanDelukin <makaev.v1d@gmail.com>
2021-01-12 18:32:16 +0400

    н

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: false,
+        draggable: true,
+        snapOnRelease: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+});
+var swiper1 = new Swiper('.dress-swiper', {
+    slidesPerView: 'auto',
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        hide: false,
+        draggable: true,
+        snapOnRelease: false,
+    },
+});
+var swiper2 = new Swiper('.prim-swiper', {
+    slidesPerView: 'auto',
+});
+var swiper3 = new Swiper('.top-swiper', {
+    slidesPerView: 'auto',
+});
+var swiper4 = new Swiper('.choose-swiper', {
+    slidesPerView: 'auto',
+});
+var swiper5 = new Swiper('.brand-swiper', {
+    slidesPerView: 'auto',
+});
